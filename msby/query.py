304ddcb03051a0669136b2dd20c0c9eb376652e1
tosh223 <rev.to12@gmail.com>
2021-10-25 22:27:32 +0900

    use classmethod

--- msby/query.py
+++ msby/query.py
@@ -6,32 +6,34 @@ def search_files(target_dir: str, condition: str):
     return [str(p) for p in path_obj.glob(condition)]
 
 class QueryParser():
-    def __init__(self, sql_path):
-        self.sql_path = sql_path
-        self.__read_query_file()
+    def __init__(self, query_str: str=None):
+        self.query_str = query_str
 
-    def __read_query_file(self):
-        with open(self.sql_path) as f:
-            query = f.read()
+    @classmethod
+    def read_file(cls, sql_path):
+        with open(sql_path) as f:
+            query_str = f.read()
+        return cls(query_str=query_str)
 
+    def parse_query(self):
         # Check the query has cte
         cte_pattern = r'(?:with|,)\s*(\w+)\s+as\s*'
-        self.__ctes = re.findall(cte_pattern, query, re.IGNORECASE)
+        ctes = re.findall(cte_pattern, self.query_str, re.IGNORECASE)
 
-        # Check row number that main query starts
-        self.__query_group = {}
-        main_pattern = r'\)[;\s]*select' if any(self.__ctes) else r'select'
-        main_row_num = re.search(main_pattern, query, re.IGNORECASE).span()[0]
+        # Check a row number that main query starts
+        main_pattern = r'\)[;\s]*select' if any(ctes) else r'select'
+        main_row_num = re.search(main_pattern, self.query_str, re.IGNORECASE).span()[0]
 
         # Separate the query to 'main' and 'cte'
-        self.__query_group['main'] = query[main_row_num:].strip()
-        self.__query_group['cte'] = query[:main_row_num].strip()
+        query_group = {}
+        query_group['main'] = self.query_str[main_row_num:].strip()
+        query_group['cte'] = self.query_str[:main_row_num].strip()
 
-    def detect_tables(self):
         table_pattern = r'(?:from|join)\s+([`.\-\w]+)'
-        main_tables_with_cte_alias = re.findall(table_pattern, self.__query_group['main'], re.IGNORECASE)
-        main_tables = [table for table in main_tables_with_cte_alias if table not in self.__ctes]
-        cte_tables = re.findall(table_pattern, self.__query_group['cte'], re.IGNORECASE)
+        main_tables_with_cte_alias = re.findall(table_pattern, query_group['main'], re.IGNORECASE)
+        main_tables = [table for table in main_tables_with_cte_alias if table not in ctes]
+        cte_tables = re.findall(table_pattern, query_group['cte'], re.IGNORECASE)
+
         return {
             'main': main_tables,
             'cte': cte_tables,
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,26 +1,39 @@
 import os
 from msby import query
 
-class TestSearchSuccess:
+class TestSearchFilesSuccess:
     def test_search_files(self):
         target_dir = os.path.dirname(__file__)
         condition = 'sql/*.sql'
         results = query.search_files(target_dir, condition)
         assert len(results) > 0
 
-    def test_detect_tables_a(self):
+class TestSearchSuccess:
+    def test_parse_query_str(self):
+        query_str = 'SELECT * FROM PROJECT_X.DATASET_X.TABLE_X INNER JOIN PROJECT_X.DATASET_X.TABLE_Y USING(ID)'
+        parser = query.QueryParser(query_str=query_str)
+        results = parser.parse_query()
+        assert results == {
+            'main': [
+                'PROJECT_X.DATASET_X.TABLE_X',
+                'PROJECT_X.DATASET_X.TABLE_Y',
+            ],
+            'cte': [],
+        }
+
+    def test_parse_query_file_a(self):
         sql_path = './tests/sql/test_a.sql'
-        parser = query.QueryParser(sql_path)
-        results = parser.detect_tables()
+        parser = query.QueryParser.read_file(sql_path=sql_path)
+        results = parser.parse_query()
         assert results == {
             'main': ['PROJECT_A.DATASET_A.TABLE_A'],
             'cte': [],
         }
 
-    def test_detect_tables_b(self):
+    def test_parse_query_file_b(self):
         sql_path = './tests/sql/test_b.sql'
-        parser = query.QueryParser(sql_path)
-        results = parser.detect_tables()
+        parser = query.QueryParser.read_file(sql_path=sql_path)
+        results = parser.parse_query()
         assert results == {
             'main': ['PROJECT_B.DATASET_B.TABLE_B'],
             'cte': [
