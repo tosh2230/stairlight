75baa020e17e07222fa053b39e09d3f2cef3c12d
tosh223 <rev.to12@gmail.com>
2021-11-07 17:30:52 +0900

    add a down test pattern

--- tests/test_strl.py
+++ tests/test_strl.py
@@ -1,3 +1,8 @@
+import os
+
+from stairlight import ResponseType
+
+
 class TestProperty:
     def test_maps(self, stair_light):
         assert len(stair_light.maps) > 0
@@ -23,6 +28,37 @@ class TestSuccess:
             "PROJECT_d.DATASET_d.TABLE_d",
         ]
 
+    def test_up_recursive_plain_table(self, stair_light):
+        table_name = "PROJECT_D.DATASET_E.TABLE_F"
+        result = stair_light.up(
+            table_name=table_name,
+            recursive=True,
+            verbose=False,
+            response_type=ResponseType.TABLE.value,
+        )
+        assert sorted(result) == [
+            "PROJECT_C.DATASET_C.TABLE_C",
+            "PROJECT_J.DATASET_K.TABLE_L",
+            "PROJECT_P.DATASET_Q.TABLE_R",
+            "PROJECT_S.DATASET_T.TABLE_U",
+            "PROJECT_V.DATASET_W.TABLE_X",
+            "PROJECT_d.DATASET_d.TABLE_d",
+        ]
+
+    def test_up_recursive_plain_file(self, stair_light):
+        table_name = "PROJECT_D.DATASET_E.TABLE_F"
+        result = stair_light.up(
+            table_name=table_name,
+            recursive=True,
+            verbose=False,
+            response_type=ResponseType.FILE.value,
+        )
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        assert sorted(result) == [
+            f"{current_dir}/sql/main/test_b.sql",
+            f"{current_dir}/sql/main/test_c.sql",
+        ]
+
     def test_down_recursive_verbose(self, stair_light):
         table_name = "PROJECT_C.DATASET_C.TABLE_C"
         result = stair_light.down(table_name=table_name, recursive=True, verbose=True)
@@ -30,4 +66,34 @@ class TestSuccess:
             "PROJECT_D.DATASET_E.TABLE_F",
             "PROJECT_G.DATASET_H.TABLE_I",
             "PROJECT_d.DATASET_e.TABLE_f",
+        ]
+
+    def test_down_recursive_plain_table(self, stair_light):
+        table_name = "PROJECT_C.DATASET_C.TABLE_C"
+        result = stair_light.down(
+            table_name=table_name,
+            recursive=True,
+            verbose=False,
+            response_type=ResponseType.TABLE.value,
+        )
+        assert sorted(result) == [
+            "PROJECT_D.DATASET_E.TABLE_F",
+            "PROJECT_G.DATASET_H.TABLE_I",
+            "PROJECT_d.DATASET_e.TABLE_f",
+            "PROJECT_j.DATASET_k.TABLE_l",
+        ]
+
+    def test_down_recursive_plain_file(self, stair_light):
+        table_name = "PROJECT_C.DATASET_C.TABLE_C"
+        result = stair_light.down(
+            table_name=table_name,
+            recursive=True,
+            verbose=False,
+            response_type=ResponseType.FILE.value,
+        )
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        assert sorted(result) == [
+            f"{current_dir}/sql/main/test_b.sql",
+            f"{current_dir}/sql/main/test_d.sql",
+            "gs://stairlight/sql/test_b/test_b.sql",
         ]
