0db02364e650370d7fdc6b3df3a7d89e8dfe40eb
tosh2230 <rev.to12@gmail.com>
2021-11-28 19:26:48 +0900

    docstring map.py

--- src/stairlight/map.py
+++ src/stairlight/map.py
@@ -3,14 +3,30 @@ from .template import SourceType, SQLTemplate, TemplateSource
 
 
 class Map:
-    def __init__(self, strl_config, map_config, mapped={}) -> None:
+    """Manages functions related to dependency map objects"""
+
+    def __init__(self, strl_config: dict, map_config: dict, mapped: dict = {}) -> None:
+        """Manages functions related to dependency map objects
+
+        Args:
+            strl_config (dict): Stairlight configuration
+            map_config (dict): Mapping configuration
+            mapped (dict, optional):
+                Mapped file attributes when a mapping configuration file loaded.
+                Defaults to {}.
+        """
         self.mapped = mapped
         self.unmapped = []
         self._template_source = TemplateSource(
             strl_config=strl_config, map_config=map_config
         )
 
-    def collect_undefined(self, sql_template):
+    def collect_undefined(self, sql_template: SQLTemplate) -> None:
+        """Create a list of unmapped file attributes
+
+        Args:
+            sql_template (SQLTemplate): SQL template
+        """
         self.unmapped.append(
             {
                 "sql_template": sql_template,
@@ -18,11 +34,13 @@ class Map:
             }
         )
 
-    def write_blank(self):
+    def write_blank(self) -> None:
+        """Create a list of unmapped file attributes, if no mapping configuration set"""
         for sql_template in self._template_source.search():
             self.collect_undefined(sql_template)
 
-    def write(self):
+    def write(self) -> None:
+        """Write a dependency map"""
         for sql_template in self._template_source.search():
             param_list = sql_template.get_param_list()
             if param_list:
@@ -31,7 +49,13 @@ class Map:
             else:
                 self._remap(sql_template=sql_template)
 
-    def _remap(self, sql_template: SQLTemplate, params: dict = {}):
+    def _remap(self, sql_template: SQLTemplate, params: dict = {}) -> None:
+        """Remap a dependency map
+
+        Args:
+            sql_template (SQLTemplate): SQL template
+            params (dict, optional): Jinja parameters. Defaults to {}.
+        """
         downstairs = sql_template.search_mapped_table(params=params)
 
         # Grep jinja template variables to suggest new configurations
