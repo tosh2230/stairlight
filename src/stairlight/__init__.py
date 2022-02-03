a5b0b183b5d52bea4e8b88b722e156441400340a
tosh2230 <rev.to12@gmail.com>
2022-02-03 22:28:30 +0900

    lint

--- src/stairlight/__init__.py
+++ src/stairlight/__init__.py
@@ -1,8 +1,3 @@
-from stairlight.stairlight import (
-    StairLight,
-    ResponseType,
-    SearchDirection,
-    is_cyclic,
-)
+from stairlight.stairlight import ResponseType, SearchDirection, StairLight, is_cyclic
 
 __all__ = ["StairLight", "ResponseType", "SearchDirection", "is_cyclic"]
