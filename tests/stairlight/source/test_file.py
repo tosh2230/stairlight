28f6c4fd238c9fac6c8fc3b3f88558b4e757dccd
tosh2230 <rev.to12@gmail.com>
2022-02-23 12:34:58 +0900

    refactoring test_file

--- tests/stairlight/source/test_file.py
+++ tests/stairlight/source/test_file.py
@@ -8,50 +8,34 @@ from src.stairlight.source.file import (
 )
 
 
-@pytest.fixture(scope="module")
-def template_source(stairlight_config, mapping_config):
-    source_attributes = {
-        config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.FILE.value,
-        config_key.FILE_SYSTEM_PATH: "./tests/sql",
-        config_key.REGEX: ".*/*.sql",
-    }
-    return FileTemplateSource(
-        stairlight_config=stairlight_config,
-        mapping_config=mapping_config,
-        source_attributes=source_attributes,
-    )
-
-
-@pytest.fixture(scope="module")
-def template_source_no_exclude(configurator, mapping_config):
-    stairlight_config = configurator.read(prefix="stairlight_no_exclude")
-    source_attributes = {
-        config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.FILE.value,
-        config_key.FILE_SYSTEM_PATH: "./tests/sql",
-        config_key.REGEX: ".*/*.sql",
-    }
-    return FileTemplateSource(
-        stairlight_config=stairlight_config,
-        mapping_config=mapping_config,
-        source_attributes=source_attributes,
-    )
-
-
 class TestFileTemplateSource:
-    def test_search_templates_iter(self, template_source):
+    @pytest.fixture(scope="class")
+    def file_template_source(self, stairlight_config, mapping_config):
+        source_attributes = {
+            config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.FILE.value,
+            config_key.FILE_SYSTEM_PATH: "./tests/sql",
+            config_key.REGEX: ".*/*.sql",
+        }
+        return FileTemplateSource(
+            stairlight_config=stairlight_config,
+            mapping_config=mapping_config,
+            source_attributes=source_attributes,
+        )
+
+    def test_search_templates_iter(self, file_template_source):
         result = []
-        for file in template_source.search_templates_iter():
+        for file in file_template_source.search_templates_iter():
             result.append(file)
         assert len(result) > 0
 
-    def test_is_excluded_one_line(self, template_source):
-        assert not template_source.is_excluded(
+    def test_is_excluded_one_line(self, file_template_source):
+        assert not file_template_source.is_excluded(
             source_type=TemplateSourceType.FILE,
             key="tests/sql/main/one_line_no_project.sql",
         )
 
-    def test_is_excluded_test_exclude(self, template_source):
-        assert template_source.is_excluded(
+    def test_is_excluded_test_exclude(self, file_template_source):
+        assert file_template_source.is_excluded(
             source_type=TemplateSourceType.FILE,
             key="tests/sql/main/exclude.sql",
         )
@@ -65,6 +49,20 @@ class TestFileTemplateSource:
     ],
 )
 class TestFileTemplateSourceNoExclude:
+    @pytest.fixture(scope="class")
+    def template_source_no_exclude(self, configurator, mapping_config):
+        stairlight_config = configurator.read(prefix="stairlight_no_exclude")
+        source_attributes = {
+            config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.FILE.value,
+            config_key.FILE_SYSTEM_PATH: "./tests/sql",
+            config_key.REGEX: ".*/*.sql",
+        }
+        return FileTemplateSource(
+            stairlight_config=stairlight_config,
+            mapping_config=mapping_config,
+            source_attributes=source_attributes,
+        )
+
     def test_is_excluded(self, template_source_no_exclude, key):
         assert not template_source_no_exclude.is_excluded(
             source_type=TemplateSourceType.FILE,
@@ -121,21 +119,32 @@ class TestFileTemplateNotMapped:
         assert len(file_template.get_jinja_params(template_str)) > 0
 
 
-class TestFileTemplateRender:
-    def test_render(self, mapping_config):
-        params = {
-            "params": {
+@pytest.mark.parametrize(
+    "key, params",
+    [
+        (
+            "tests/sql/main/cte_multi_line_params.sql",
+            {
                 "main_table": "PROJECT_P.DATASET_Q.TABLE_R",
                 "sub_table_01": "PROJECT_S.DATASET_T.TABLE_U",
                 "sub_table_02": "PROJECT_V.DATASET_W.TABLE_X",
-            }
-        }
-        sql_template = FileTemplate(
+            },
+        )
+    ],
+)
+class TestFileTemplateRender:
+    @pytest.fixture(scope="function")
+    def file_template(self, mapping_config, key):
+        return FileTemplate(
             mapping_config=mapping_config,
             source_type=TemplateSourceType.FILE,
-            key="tests/sql/main/cte_multi_line_params.sql",
+            key=key,
+            bucket=None,
         )
-        actual = sql_template.render(params=params)
+
+    def test_render(self, file_template, params):
+        params = {"params": params}
+        actual = file_template.render(params=params)
         expected = """WITH c AS (
     SELECT
         test_id,
