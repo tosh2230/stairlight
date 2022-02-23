0c045a217750af9031d4e0f4c8577dfcb18e9639
tosh2230 <rev.to12@gmail.com>
2022-02-23 13:41:40 +0900

    refactoring test_gcs

--- tests/stairlight/source/test_gcs.py
+++ tests/stairlight/source/test_gcs.py
@@ -1,29 +1,58 @@
-import src.stairlight.config as config
+import pytest
+
 from src.stairlight import config_key
 from src.stairlight.source.gcs import GcsTemplate, GcsTemplateSource, TemplateSourceType
 
 
-class TestSQLTemplate:
-    configurator = config.Configurator(dir="tests/config")
-    mapping_config = configurator.read(prefix=config_key.MAPPING_CONFIG_FILE_PREFIX)
-    sql_template = GcsTemplate(
-        mapping_config=mapping_config,
-        source_type=TemplateSourceType.GCS,
-        key="sql/cte/cte_multi_line.sql",
-        bucket="stairlight",
-    )
+class TestGcsTemplateSource:
+    @pytest.fixture(scope="class")
+    def gcs_template_source(self, stairlight_config, mapping_config):
+        source_attributes = {
+            config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.GCS.value,
+            config_key.PROJECT_ID: None,
+            config_key.BUCKET_NAME: "stairlight",
+            config_key.REGEX: "sql/.*/*.sql",
+        }
+        return GcsTemplateSource(
+            stairlight_config=stairlight_config,
+            mapping_config=mapping_config,
+            source_attributes=source_attributes,
+        )
 
-    def test_is_mapped(self):
-        assert self.sql_template.is_mapped()
+    def test_search_templates_iter(self, gcs_template_source):
+        result = []
+        for file in gcs_template_source.search_templates_iter():
+            result.append(file)
+        assert len(result) > 0
 
-    def test_get_jinja_params(self):
-        template_str = self.sql_template.get_template_str()
-        assert len(self.sql_template.get_jinja_params(template_str)) > 0
 
-    def test_get_uri(self):
-        assert self.sql_template.uri == "gs://stairlight/sql/cte/cte_multi_line.sql"
+@pytest.mark.parametrize(
+    "bucket, key",
+    [
+        ("stairlight", "sql/cte/cte_multi_line.sql"),
+    ],
+)
+class TestGcsTemplate:
+    @pytest.fixture(scope="function")
+    def gcs_template(self, mapping_config, bucket, key):
+        return GcsTemplate(
+            mapping_config=mapping_config,
+            source_type=TemplateSourceType.GCS,
+            bucket=bucket,
+            key=key,
+        )
 
-    def test_render(self):
+    def test_is_mapped(self, gcs_template):
+        assert gcs_template.is_mapped()
+
+    def test_get_jinja_params(self, gcs_template):
+        template_str = gcs_template.get_template_str()
+        assert len(gcs_template.get_jinja_params(template_str)) > 0
+
+    def test_get_uri(self, gcs_template):
+        assert gcs_template.uri == "gs://stairlight/sql/cte/cte_multi_line.sql"
+
+    def test_render(self, gcs_template):
         params = {
             "params": {
                 "PROJECT": "PROJECT_g",
@@ -60,31 +89,5 @@ FROM
         ON b.test_id = d.test_id
 WHERE
     1 = 1"""
-        actual = self.sql_template.render(params=params)
+        actual = gcs_template.render(params=params)
         assert actual == expected
-
-
-class TestGcsTemplateSource:
-    configurator = config.Configurator(dir="tests/config")
-    stairlight_config = configurator.read(
-        prefix=config_key.STAIRLIGHT_CONFIG_FILE_PREFIX
-    )
-    mapping_config = configurator.read(prefix=config_key.MAPPING_CONFIG_FILE_PREFIX)
-
-    source_attributes = {
-        config_key.TEMPLATE_SOURCE_TYPE: TemplateSourceType.GCS.value,
-        config_key.PROJECT_ID: None,
-        config_key.BUCKET_NAME: "stairlight",
-        config_key.REGEX: "sql/.*/*.sql",
-    }
-    template_source = GcsTemplateSource(
-        stairlight_config=stairlight_config,
-        mapping_config=mapping_config,
-        source_attributes=source_attributes,
-    )
-
-    def test_search_templates_iter(self):
-        result = []
-        for file in self.template_source.search_templates_iter():
-            result.append(file)
-        assert len(result) > 0
