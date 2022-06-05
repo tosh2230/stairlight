6b2fde49595e2153a27c3317d6bd928b2d4ab05c
tosh2230 <rev.to12@gmail.com>
2022-06-05 22:45:15 +0900

    parametrize

--- tests/stairlight/test_query.py
+++ tests/stairlight/test_query.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.stairlight import map_key
 from src.stairlight.query import Query, solve_table_prefix
 
@@ -31,184 +33,208 @@ class TestSuccess:
             },
         ]
 
-    def test_parse_cte_one_line(self):
-        with open("tests/sql/query/cte_one_line.sql") as f:
+    @pytest.mark.parametrize(
+        "file, expected",
+        [
+            (
+                "tests/sql/query/cte_one_line.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
+                        map_key.LINE_NUMBER: 1,
+                        map_key.LINE_STRING: (
+                            "WITH c AS (SELECT test_id, col_c "
+                            "FROM PROJECT_C.DATASET_C.TABLE_C WHERE 0 = 0),"
+                            "d AS ("
+                            "SELECT test_id, col_d "
+                            "FROM PROJECT_d.DATASET_d.TABLE_d "
+                            "WHERE 0 = 0) "
+                            "SELECT * FROM PROJECT_B.DATASET_B.TABLE_B AS b "
+                            "INNER JOIN c ON b.test_id = c.test_id "
+                            "INNER JOIN d ON b.test_id = d.test_id WHERE 1 = 1"
+                        ),
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
+                        map_key.LINE_NUMBER: 1,
+                        map_key.LINE_STRING: (
+                            "WITH c AS (SELECT test_id, col_c "
+                            "FROM PROJECT_C.DATASET_C.TABLE_C WHERE 0 = 0),"
+                            "d AS ("
+                            "SELECT test_id, col_d "
+                            "FROM PROJECT_d.DATASET_d.TABLE_d "
+                            "WHERE 0 = 0) "
+                            "SELECT * FROM PROJECT_B.DATASET_B.TABLE_B AS b "
+                            "INNER JOIN c ON b.test_id = c.test_id "
+                            "INNER JOIN d ON b.test_id = d.test_id WHERE 1 = 1"
+                        ),
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
+                        map_key.LINE_NUMBER: 1,
+                        map_key.LINE_STRING: (
+                            "WITH c AS (SELECT test_id, col_c "
+                            "FROM PROJECT_C.DATASET_C.TABLE_C WHERE 0 = 0),"
+                            "d AS ("
+                            "SELECT test_id, col_d "
+                            "FROM PROJECT_d.DATASET_d.TABLE_d "
+                            "WHERE 0 = 0) "
+                            "SELECT * FROM PROJECT_B.DATASET_B.TABLE_B AS b "
+                            "INNER JOIN c ON b.test_id = c.test_id "
+                            "INNER JOIN d ON b.test_id = d.test_id WHERE 1 = 1"
+                        ),
+                    },
+                ],
+            ),
+            (
+                "tests/sql/query/cte_multi_line.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
+                        map_key.LINE_NUMBER: 23,
+                        map_key.LINE_STRING: "    PROJECT_B.DATASET_B.TABLE_B AS b",
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
+                        map_key.LINE_NUMBER: 6,
+                        map_key.LINE_STRING: "        PROJECT_C.DATASET_C.TABLE_C",
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
+                        map_key.LINE_NUMBER: 15,
+                        map_key.LINE_STRING: "        PROJECT_d.DATASET_d.TABLE_d",
+                    },
+                ],
+            ),
+            (
+                "tests/sql/query/nested_join.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
+                        map_key.LINE_NUMBER: 4,
+                        map_key.LINE_STRING: "    PROJECT_B.DATASET_B.TABLE_B AS b",
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
+                        map_key.LINE_NUMBER: 10,
+                        map_key.LINE_STRING: "            PROJECT_C.DATASET_C.TABLE_C",
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
+                        map_key.LINE_NUMBER: 20,
+                        map_key.LINE_STRING: (
+                            "            PROJECT_d.DATASET_d.TABLE_d d"
+                        ),
+                    },
+                    {
+                        map_key.TABLE_NAME: "PROJECT_e.DATASET_e.TABLE_e",
+                        map_key.LINE_NUMBER: 21,
+                        map_key.LINE_STRING: (
+                            "            LEFT OUTER JOIN PROJECT_e.DATASET_e.TABLE_e"
+                        ),
+                    },
+                ],
+            ),
+            (
+                "tests/sql/query/union_same_table.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: (
+                            "test_project.beam_streaming.taxirides_realtime"
+                        ),
+                        map_key.LINE_NUMBER: 6,
+                        map_key.LINE_STRING: (
+                            "    test_project.beam_streaming.taxirides_realtime"
+                        ),
+                    },
+                    {
+                        map_key.TABLE_NAME: (
+                            "test_project.beam_streaming.taxirides_realtime"
+                        ),
+                        map_key.LINE_NUMBER: 15,
+                        map_key.LINE_STRING: (
+                            "    test_project.beam_streaming.taxirides_realtime"
+                        ),
+                    },
+                ],
+            ),
+            (
+                "tests/sql/query/cte_multi_tables_01.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_A",
+                        map_key.LINE_NUMBER: 6,
+                        map_key.LINE_STRING: "		project.dataset.table_test_A",
+                    },
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_B",
+                        map_key.LINE_NUMBER: 13,
+                        map_key.LINE_STRING: "		project.dataset.table_test_B AS test_B",
+                    },
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_C",
+                        map_key.LINE_NUMBER: 19,
+                        map_key.LINE_STRING: (
+                            "FROM project.dataset.table_test_C AS test_C"
+                        ),
+                    },
+                ],
+            ),
+            (
+                "tests/sql/query/cte_multi_tables_02.sql",
+                [
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_A",
+                        map_key.LINE_NUMBER: 6,
+                        map_key.LINE_STRING: (
+                            "		project.dataset.table_test_A -- table_test_B"
+                        ),
+                    },
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_B",
+                        map_key.LINE_NUMBER: 12,
+                        map_key.LINE_STRING: "		project.dataset.table_test_B AS test_B",
+                    },
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_C",
+                        map_key.LINE_NUMBER: 19,
+                        map_key.LINE_STRING: "		project.dataset.table_test_C AS test_C",
+                    },
+                    {
+                        map_key.TABLE_NAME: "project.dataset.table_test_D",
+                        map_key.LINE_NUMBER: 26,
+                        map_key.LINE_STRING: (
+                            "FROM project.dataset.table_test_D AS test_D"
+                        ),
+                    },
+                ],
+            ),
+        ],
+    )
+    def test_detect_upstairs_attributes(self, file, expected):
+        with open(file) as f:
             query_str = f.read()
         query = Query(query_str=query_str)
-        results = []
+        actual = []
         for result in query.detect_upstairs_attributes():
-            results.append(result)
-        assert results == [
-            {
-                map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
-                map_key.LINE_NUMBER: 1,
-                map_key.LINE_STRING: query_str,
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
-                map_key.LINE_NUMBER: 1,
-                map_key.LINE_STRING: query_str,
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
-                map_key.LINE_NUMBER: 1,
-                map_key.LINE_STRING: query_str,
-            },
-        ]
+            actual.append(result)
+        assert actual == expected
 
-    def test_parse_cte_multi_line(self):
-        with open("tests/sql/query/cte_multi_line.sql") as f:
-            query_str = f.read()
-        query = Query(query_str=query_str)
-        results = []
-        for result in query.detect_upstairs_attributes():
-            results.append(result)
-        assert results == [
-            {
-                map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
-                map_key.LINE_NUMBER: 23,
-                map_key.LINE_STRING: "    PROJECT_B.DATASET_B.TABLE_B AS b",
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
-                map_key.LINE_NUMBER: 6,
-                map_key.LINE_STRING: "        PROJECT_C.DATASET_C.TABLE_C",
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
-                map_key.LINE_NUMBER: 15,
-                map_key.LINE_STRING: "        PROJECT_d.DATASET_d.TABLE_d",
-            },
-        ]
-
-    def test_parse_nested_join(self):
-        with open("tests/sql/query/nested_join.sql") as f:
-            query_str = f.read()
-        query = Query(query_str=query_str)
-        results = []
-        for result in query.detect_upstairs_attributes():
-            results.append(result)
-        lines_str_21 = "            LEFT OUTER JOIN PROJECT_e.DATASET_e.TABLE_e"
-        assert results == [
-            {
-                map_key.TABLE_NAME: "PROJECT_B.DATASET_B.TABLE_B",
-                map_key.LINE_NUMBER: 4,
-                map_key.LINE_STRING: "    PROJECT_B.DATASET_B.TABLE_B AS b",
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_C.DATASET_C.TABLE_C",
-                map_key.LINE_NUMBER: 10,
-                map_key.LINE_STRING: "            PROJECT_C.DATASET_C.TABLE_C",
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_d.DATASET_d.TABLE_d",
-                map_key.LINE_NUMBER: 20,
-                map_key.LINE_STRING: "            PROJECT_d.DATASET_d.TABLE_d d",
-            },
-            {
-                map_key.TABLE_NAME: "PROJECT_e.DATASET_e.TABLE_e",
-                map_key.LINE_NUMBER: 21,
-                map_key.LINE_STRING: lines_str_21,
-            },
-        ]
-
-    def test_union_same_table(self):
-        with open("tests/sql/query/union_same_table.sql") as f:
-            query_str = f.read()
-        query = Query(query_str=query_str)
-        results = []
-        for result in query.detect_upstairs_attributes():
-            results.append(result)
-        line_str_6 = "    test_project.beam_streaming.taxirides_realtime"
-        line_str_15 = "    test_project.beam_streaming.taxirides_realtime"
-        assert results == [
-            {
-                map_key.TABLE_NAME: "test_project.beam_streaming.taxirides_realtime",
-                map_key.LINE_NUMBER: 6,
-                map_key.LINE_STRING: line_str_6,
-            },
-            {
-                map_key.TABLE_NAME: "test_project.beam_streaming.taxirides_realtime",
-                map_key.LINE_NUMBER: 15,
-                map_key.LINE_STRING: line_str_15,
-            },
-        ]
-
-    def test_parse_cte_multi_tables_01(self):
-        with open("tests/sql/query/cte_multi_tables_01.sql") as f:
-            query_str = f.read()
-        query = Query(query_str=query_str)
-        results = []
-        for result in query.detect_upstairs_attributes():
-            results.append(result)
-        assert results == [
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_A",
-                map_key.LINE_NUMBER: 6,
-                map_key.LINE_STRING: "		project.dataset.table_test_A",
-            },
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_B",
-                map_key.LINE_NUMBER: 13,
-                map_key.LINE_STRING: "		project.dataset.table_test_B AS test_B",
-            },
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_C",
-                map_key.LINE_NUMBER: 19,
-                map_key.LINE_STRING: "FROM project.dataset.table_test_C AS test_C",
-            },
-        ]
-
-    def test_parse_cte_multi_tables_02(self):
-        with open("tests/sql/query/cte_multi_tables_02.sql") as f:
-            query_str = f.read()
-        query = Query(query_str=query_str)
-        results = []
-        for result in query.detect_upstairs_attributes():
-            results.append(result)
-        assert results == [
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_A",
-                map_key.LINE_NUMBER: 6,
-                map_key.LINE_STRING: "		project.dataset.table_test_A -- table_test_B",
-            },
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_B",
-                map_key.LINE_NUMBER: 12,
-                map_key.LINE_STRING: "		project.dataset.table_test_B AS test_B",
-            },
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_C",
-                map_key.LINE_NUMBER: 19,
-                map_key.LINE_STRING: "		project.dataset.table_test_C AS test_C",
-            },
-            {
-                map_key.TABLE_NAME: "project.dataset.table_test_D",
-                map_key.LINE_NUMBER: 26,
-                map_key.LINE_STRING: "FROM project.dataset.table_test_D AS test_D",
-            },
-        ]
-
-    def test_solve_table_prefix_one(self):
-        table = "DATASET_d.TABLE_d"
-        default_table_prefix = "PROJECT_A"
-        assert (
-            solve_table_prefix(table=table, default_table_prefix=default_table_prefix)
-            == "PROJECT_A.DATASET_d.TABLE_d"
+    @pytest.mark.parametrize(
+        "table, default_table_prefix, expected",
+        [
+            ("DATASET_d.TABLE_d", "PROJECT_A", "PROJECT_A.DATASET_d.TABLE_d"),
+            ("TABLE_d", "PROJECT_A.DATASET_A", "PROJECT_A.DATASET_A.TABLE_d"),
+            ("PROJECT_d.DATASET_d.TABLE_d", "PROJECT_A", "PROJECT_d.DATASET_d.TABLE_d"),
+        ],
+    )
+    def test_solve_table_prefix(
+        self,
+        table: str,
+        default_table_prefix: str,
+        expected: str,
+    ):
+        actual = solve_table_prefix(
+            table=table, default_table_prefix=default_table_prefix
         )
-
-    def test_solve_table_prefix_two(self):
-        table = "TABLE_d"
-        default_table_prefix = "PROJECT_A.DATASET_A"
-        assert (
-            solve_table_prefix(table=table, default_table_prefix=default_table_prefix)
-            == "PROJECT_A.DATASET_A.TABLE_d"
-        )
-
-    def test_solve_table_prefix_nothing(self):
-        table = "PROJECT_d.DATASET_d.TABLE_d"
-        default_table_prefix = "PROJECT_A"
-        assert (
-            solve_table_prefix(table=table, default_table_prefix=default_table_prefix)
-            == "PROJECT_d.DATASET_d.TABLE_d"
-        )
+        assert actual == expected
