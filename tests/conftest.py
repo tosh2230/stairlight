8c5d2fc4e96f22aa22933d67be40c94823036e51
tosh2230 <rev.to12@gmail.com>
2022-06-06 20:43:41 +0900

    mv fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,6 @@ import pytest
 
 from src.stairlight import config_key
 from src.stairlight.config import Configurator
-from src.stairlight.map import Map
 from src.stairlight.stairlight import StairLight
 
 
@@ -26,40 +25,8 @@ def mapping_config(configurator: Configurator) -> dict:
 
 
 @pytest.fixture(scope="session")
-def dependency_map(stairlight_config: dict, mapping_config: dict) -> Map:
-    dependency_map = Map(
-        stairlight_config=stairlight_config, mapping_config=mapping_config
-    )
-    dependency_map.write()
-    return dependency_map
-
-
-@pytest.fixture(scope="session")
-def tests_dir() -> str:
+def tests_abspath() -> str:
     return os.path.dirname(os.path.abspath(__file__))
-
-
-@pytest.fixture(scope="session")
-def stairlight_init() -> Iterator[StairLight]:
-    stairlight = StairLight(config_dir="tests/config/test_init")
-    stairlight.create_map()
-    yield stairlight
-    teardown_rm_file(file="tests/config/test_init/stairlight.yaml")
-
-
-@pytest.fixture(scope="session")
-def stairlight_check() -> Iterator[StairLight]:
-    stairlight = StairLight(config_dir="tests/config/test_check")
-    stairlight.create_map()
-    yield stairlight
-    teardown_rm_config(pathname="tests/config/test_check/mapping_*.yaml")
-
-
-@pytest.fixture(scope="session")
-def stairlight_check_no_file_found() -> Iterator[StairLight]:
-    stairlight = StairLight(config_dir="tests/config/test_check_no_file_found")
-    stairlight.create_map()
-    yield stairlight
 
 
 @pytest.fixture(scope="session")
@@ -77,19 +44,17 @@ def stairlight_save(save_file="./tests/test_save_map.json") -> Iterator[StairLig
 
 
 @pytest.fixture(scope="session")
-def stairlight_load_and_save() -> Iterator[StairLight]:
-    load_files = [
-        "./tests/results/file_01.json",
-        "./tests/results/file_02.json",
-        "./tests/results/gcs.json",
-    ]
-    save_file = "./tests/results/actual.json"
-    stairlight = StairLight(
-        config_dir="tests/config", load_files=load_files, save_file=save_file
-    )
-    stairlight.create_map()
-    yield stairlight
-    teardown_rm_file(save_file)
+def stairlight_template_prefix() -> Iterator[str]:
+    prefix = "pytest_stairlight"
+    yield prefix
+    teardown_rm_config(pathname=f"tests/config/{prefix}*.yaml")
+
+
+@pytest.fixture(scope="session")
+def mapping_template_prefix() -> Iterator[str]:
+    prefix = "pytest_mapping"
+    yield prefix
+    teardown_rm_config(pathname=f"tests/config/{prefix}*.yaml")
 
 
 def teardown_rm_file(file: str) -> None:
@@ -101,17 +66,3 @@ def teardown_rm_config(pathname: str) -> None:
     rm_files = glob.glob(pathname=pathname)
     for file in rm_files:
         os.remove(file)
-
-
-@pytest.fixture
-def stairlight_template_prefix() -> Iterator[str]:
-    prefix = "pytest_stairlight"
-    yield prefix
-    teardown_rm_config(pathname=f"tests/config/{prefix}*.yaml")
-
-
-@pytest.fixture
-def mapping_template_prefix() -> Iterator[str]:
-    prefix = "pytest_mapping"
-    yield prefix
-    teardown_rm_config(pathname=f"tests/config/{prefix}*.yaml")
