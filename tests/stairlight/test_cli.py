a080c5bb0b6e7991e857ec5cca1af76a29601fcf
tosh2230 <rev.to12@gmail.com>
2022-06-14 20:18:29 +0900

    format

--- tests/stairlight/test_cli.py
+++ tests/stairlight/test_cli.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Iterator, Union
+from typing import Any, Dict, Iterator, List, Union
 
 import pytest
 
--- tests/stairlight/test_stairlight.py
+++ tests/stairlight/test_stairlight.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Dict, List, Iterator
+from typing import Any, Dict, Iterator, List
 
 import pytest
 
