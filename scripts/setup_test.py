9024b665a80866cf2009fda2e9d8c6f3b123adae
tosh2230 <rev.to12@gmail.com>
2022-06-25 10:00:21 +0900

    type check

--- scripts/setup_test.py
+++ scripts/setup_test.py
@@ -13,18 +13,18 @@ s3_client = boto3.resource("s3")
 s3_bucket = s3_client.Bucket(BUCKET_NAME)
 
 
-def get_object_key(path: str) -> str:
+def get_object_key(path: Path) -> str:
     file_name = str(path).replace(COPY_PATH, "")
     return f"sql/{file_name}"
 
 
-def upload_to_gcs(path: str) -> None:
+def upload_to_gcs(path: Path) -> None:
     object_key = get_object_key(path=path)
     blob = gcs_bucket.blob(object_key)
     blob.upload_from_filename(path)
 
 
-def upload_to_s3(path: str) -> None:
+def upload_to_s3(path: Path) -> None:
     object_key = get_object_key(path=path)
     s3_bucket.upload_file(str(path), object_key)
 
