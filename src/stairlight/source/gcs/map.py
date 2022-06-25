a17c80c805e4fa3c6b7e2550a1d771c75e88606c
tosh2230 <rev.to12@gmail.com>
2022-06-25 18:30:18 +0900

    resolve import errors

---
+++ src/stairlight/source/gcs/map.py
@@ -0,0 +1,12 @@
+from google.cloud.storage import Blob, Client
+
+from ..config_key import GCS_URI_SCHEME
+
+
+def get_gcs_blob(uri: str) -> Blob:
+    bucket_name = uri.replace(GCS_URI_SCHEME, "").split("/")[0]
+    key = uri.replace(f"{GCS_URI_SCHEME}{bucket_name}/", "")
+
+    client = Client(credentials=None, project=None)
+    bucket = client.get_bucket(bucket_name)
+    return bucket.blob(key)
---
+++ src/stairlight/source/s3/map.py
@@ -0,0 +1,12 @@
+import boto3
+from mypy_boto3_s3.service_resource import Object, S3ServiceResource
+
+from ..config_key import S3_URI_SCHEME
+
+
+def get_s3_object(uri: str) -> Object:
+    bucket_name = uri.replace(S3_URI_SCHEME, "").split("/")[0]
+    key = uri.replace(f"{S3_URI_SCHEME}{bucket_name}/", "")
+
+    s3: S3ServiceResource = boto3.resource("s3")
+    return s3.Object(bucket_name=bucket_name, key=key)
