1c37b30ecff5239c83fa2bd504cba71294a9aa6c
tosh2230 <rev.to12@gmail.com>
2023-02-25 12:04:30 +0900

    MappingFilesRegex

--- src/stairlight/__init__.py
+++ src/stairlight/__init__.py
@@ -2,8 +2,12 @@ from src.stairlight.stairlight import (
     ResponseType,
     SearchDirection,
     StairLight,
-    is_cyclic,
 )
 
 __version__ = "0.7.1"
-__all__ = ["StairLight", "ResponseType", "SearchDirection", "is_cyclic", "__version__"]
+__all__ = [
+    "ResponseType",
+    "SearchDirection",
+    "StairLight",
+    "__version__",
+]
